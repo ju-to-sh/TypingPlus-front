e82ed3fdd511bbf2d071ef33fa5dd19469de5460
ju-to-sh <088t3748@gmail.com>
2024-05-31 18:32:14 +0900

    バックエンドとの疎通確認

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const testMethod = () => {
+    axios
+      .get(`http://localhost:3001/`)
+      .then((res) => {
+        if (res) {
+          console.log(res.data)
+        }
+      })
+      .catch(() => console.log("error"));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => testMethod()}>データ取得</button>
     </div>
-  );
+  )
 }
 
 export default App;
