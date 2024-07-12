094a7d0c2ccad4cccc1feca30348885d63451939
ju-to-sh <088t3748@gmail.com>
2024-07-13 01:33:46 +0900

    マイページに前画面に戻るボタンを追加

--- app/src/components/pages/Profile.tsx
+++ app/src/components/pages/Profile.tsx
@@ -1,9 +1,12 @@
-import { Grid, Typography } from "@mui/material";
+import { Button, Grid, Typography } from "@mui/material";
 import { FC, memo } from "react";
 import { Logo } from "../atoms/Logo";
 import { UserInfoTable } from "../organisms/layout/UserInfoTable";
+import { useNavigate } from "react-router-dom";
 
 export const Profile: FC = memo(() => {
+  const navigate = useNavigate();
+
   return (
     <Grid container direction="row" sx={{ maxWidth: 600 }} margin="0 auto" justifyContent="center" alignItems="center" height="100vh">
       <Grid item xs={10} textAlign="center" margin="0 auto">
@@ -12,6 +15,9 @@ export const Profile: FC = memo(() => {
           マイページ
         </Typography>
         <UserInfoTable />
+        <Button variant="contained" color="primary" onClick={() => navigate(-1)} sx={{ mt: "24px" }}>
+          前画面に戻る
+        </Button>
       </Grid>
     </Grid>
   );
