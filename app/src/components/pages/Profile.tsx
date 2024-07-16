273b3b53e3222f8b0238c170be8e4af452c19199
ju-to-sh <088t3748@gmail.com>
2024-07-17 06:35:52 +0900

    未ログインの場合、トップ画面にリダイレクトするように修正

--- app/src/components/pages/Profile.tsx
+++ app/src/components/pages/Profile.tsx
@@ -2,23 +2,31 @@ import { Button, Grid, Typography } from "@mui/material";
 import { FC, memo } from "react";
 import { Logo } from "../atoms/Logo";
 import { UserInfoTable } from "../organisms/layout/UserInfoTable";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Navigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 export const Profile: FC = memo(() => {
   const navigate = useNavigate();
+  const [cookies] = useCookies(["accesstoken"]);
 
   return (
-    <Grid container direction="row" sx={{ maxWidth: 600 }} margin="0 auto" justifyContent="center" alignItems="center" height="100vh">
-      <Grid item xs={10} textAlign="center" margin="0 auto">
-        <Logo width={270} />
-        <Typography variant="h4" gutterBottom mb={5} mt={5}>
-          マイページ
-        </Typography>
-        <UserInfoTable />
-        <Button variant="contained" color="primary" onClick={() => navigate(-1)} sx={{ mt: "24px" }}>
-          前画面に戻る
-        </Button>
-      </Grid>
-    </Grid>
+    <>
+      {cookies.accesstoken ? (
+        <Grid container direction="row" sx={{ maxWidth: 600 }} margin="0 auto" justifyContent="center" alignItems="center" height="100vh">
+          <Grid item xs={10} textAlign="center" margin="0 auto">
+            <Logo width={270} />
+            <Typography variant="h4" gutterBottom mb={5} mt={5}>
+              マイページ
+            </Typography>
+            <UserInfoTable />
+            <Button variant="contained" color="primary" onClick={() => navigate(-1)} sx={{ mt: "24px" }}>
+              前画面に戻る
+            </Button>
+          </Grid>
+        </Grid>
+      ) : (
+        <Navigate replace to="/" />
+      )}
+    </>
   );
 });
