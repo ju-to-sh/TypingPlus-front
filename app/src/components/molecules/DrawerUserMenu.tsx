49881d620f27c2215a3800958f2dd0c620fa4c4d
ju-to-sh <088t3748@gmail.com>
2024-07-26 20:16:50 +0900

    ヘッダーのレスポンシブ対応

---
+++ app/src/components/molecules/DrawerUserMenu.tsx
@@ -0,0 +1,45 @@
+import { FC, memo } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import { Link, ListItem, ListItemButton } from "@mui/material";
+import { useCookies } from "react-cookie";
+import { useApi } from "../../hooks/useApi";
+import { useNavigate } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
+import { flashState } from "../../store/flashState";
+import { useRecoilValue } from "recoil";
+import { userIdState } from "../../store/userIdState";
+import VideogameAssetIcon from "@mui/icons-material/VideogameAsset";
+import LogoutIcon from "@mui/icons-material/Logout";
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+
+export const DrawerUserMenu: FC = memo(() => {
+  const [, , removeCookie] = useCookies(["accesstoken"]);
+  const userId = useRecoilValue(userIdState);
+
+  const navigate = useNavigate();
+  const setFlash = useSetRecoilState(flashState);
+
+  const LogoutHandler = async () => {
+    await useApi.post("/logout");
+    removeCookie("accesstoken");
+    navigate("/");
+    setTimeout(() => setFlash(false), 1000);
+  };
+
+  return (
+    <>
+      <ListItem disablePadding>
+        <ListItemButton sx={{ display: "flex", justifyContent: "center", alignItems: "center" }} component={RouterLink} to={`/users/${userId}`}>
+          <ManageAccountsIcon sx={{ marginRight: "8px", color: "#c52f24" }} />
+          <Link underline="none">マイページ</Link>
+        </ListItemButton>
+      </ListItem>
+      <ListItem disablePadding>
+        <ListItemButton sx={{ display: "flex", justifyContent: "center", alignItems: "center" }} onClick={LogoutHandler}>
+          <LogoutIcon sx={{ marginRight: "8px", color: "#c52f24" }} />
+          <Link underline="none">ログアウト</Link>
+        </ListItemButton>
+      </ListItem>
+    </>
+  );
+});
--- app/src/components/organisms/layout/Header.tsx
+++ app/src/components/organisms/layout/Header.tsx
@@ -1,45 +1,133 @@
-import { FC, memo } from "react";
-import { AppBar, Box, Link, Stack } from "@mui/material";
+import { FC, memo, useState } from "react";
+import { AppBar, Box, Drawer, IconButton, Link, List, ListItem, ListItemButton, Stack } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import VideogameAssetIcon from "@mui/icons-material/VideogameAsset";
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
+import SearchIcon from "@mui/icons-material/Search";
+import SaveAsIcon from "@mui/icons-material/SaveAs";
+import LoginIcon from "@mui/icons-material/Login";
 import { Link as RouterLink } from "react-router-dom";
 import { Logo } from "../../atoms/Logo";
 import { useCookies } from "react-cookie";
 import { UserMenu } from "../../molecules/UserMenu";
+import { DrawerUserMenu } from "../../molecules/DrawerUserMenu";
 
-export const Header: FC = memo(() => {
+type Props = {
+  window?: () => Window;
+};
+
+export const Header: FC<Props> = memo((props) => {
+  const drawerWidth = 240;
   const [cookie] = useCookies(["accesstoken"]);
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ alignItems: "center" }}>
+      <List sx={{ paddingTop: "24px" }}>
+        <ListItem disablePadding>
+          <ListItemButton sx={{ display: "flex", justifyContent: "center", alignItems: "center" }} component={RouterLink} to="/games">
+            <VideogameAssetIcon sx={{ marginRight: "8px", color: "#c52f24" }} />
+            <Link underline="none">ゲーム選択</Link>
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton sx={{ display: "flex", justifyContent: "center", alignItems: "center" }} component={RouterLink} to="/ranking">
+            <EmojiEventsIcon sx={{ marginRight: "8px", color: "#c52f24" }} />
+            <Link underline="none">ランキング一覧</Link>
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton sx={{ display: "flex", justifyContent: "center", alignItems: "center" }} component={RouterLink} to="/search">
+            <SearchIcon sx={{ marginRight: "8px", color: "#c52f24" }} />
+            <Link underline="none">問題検索</Link>
+          </ListItemButton>
+        </ListItem>
+        {cookie.accesstoken ? (
+          <DrawerUserMenu />
+        ) : (
+          <>
+            <ListItem disablePadding>
+              <ListItemButton sx={{ display: "flex", justifyContent: "center", alignItems: "center" }} component={RouterLink} to="/signup">
+                <SaveAsIcon sx={{ marginRight: "8px", color: "#c52f24" }} />
+                <Link underline="none">新規登録</Link>
+              </ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton sx={{ display: "flex", justifyContent: "center", alignItems: "center" }} component={RouterLink} to="/login">
+                <LoginIcon sx={{ marginRight: "8px", color: "#c52f24" }} />
+                <Link underline="none">ログイン</Link>
+              </ListItemButton>
+            </ListItem>
+          </>
+        )}
+      </List>
+    </Box>
+  );
+  const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <AppBar component="header" position="fixed" style={{ backgroundColor: "#ffeded", borderBottom: "2px solid #c52f24" }}>
-      <Box mt={1} mr={2} ml={2}>
-        <Stack direction="row" justifyContent="space-between" alignItems="center">
-          <Link component={RouterLink} to="/">
-            <Logo width={220} />
-          </Link>
-          <Stack lineHeight="70px" direction="row" justifyContent="flex-center" alignItems="center" spacing={3}>
-            <Link component={RouterLink} to="/games" underline="hover">
-              <div>ゲーム選択</div>
+    <>
+      <AppBar component="header" position="fixed" style={{ backgroundColor: "#ffeded", borderBottom: "2px solid #c52f24" }}>
+        <Box mt={1} mr={2} ml={2} sx={{ display: { sm: "block", md: "none" } }}>
+          <Stack direction="row" justifyContent="space-between" alignItems="center">
+            <Link component={RouterLink} to="/">
+              <Logo width={220} />
             </Link>
-            <Link component={RouterLink} to="/ranking" underline="hover">
-              <div>ランキング一覧</div>
-            </Link>
-            <Link component={RouterLink} to="/ranking" underline="hover">
-              <div>問題検索</div>
-            </Link>
-            {cookie.accesstoken ? (
-              <UserMenu />
-            ) : (
-              <>
-                <Link component={RouterLink} to="/signup" underline="hover">
-                  <div>新規会員登録</div>
-                </Link>
-                <Link component={RouterLink} to="/login" underline="hover">
-                  <div>ログイン</div>
-                </Link>
-              </>
-            )}
+            <IconButton color="inherit" aria-label="open drawer" edge="start" onClick={handleDrawerToggle} sx={{ mr: 2, display: { md: "block" } }}>
+              <MenuIcon sx={{ color: "#c52f24" }} />
+            </IconButton>
           </Stack>
-        </Stack>
-      </Box>
-    </AppBar>
+        </Box>
+        <Box mt={1} mr={2} ml={2} sx={{ display: { xs: "none", sm: "none", md: "block" } }}>
+          <Stack direction="row" justifyContent="space-between" alignItems="center">
+            <Link component={RouterLink} to="/">
+              <Logo width={220} />
+            </Link>
+            <Stack lineHeight="70px" direction="row" justifyContent="flex-center" alignItems="center" spacing={3}>
+              <Link component={RouterLink} to="/games" underline="hover">
+                <div>ゲーム選択</div>
+              </Link>
+              <Link component={RouterLink} to="/ranking" underline="hover">
+                <div>ランキング一覧</div>
+              </Link>
+              <Link component={RouterLink} to="/search" underline="hover">
+                <div>問題検索</div>
+              </Link>
+              {cookie.accesstoken ? (
+                <UserMenu />
+              ) : (
+                <>
+                  <Link component={RouterLink} to="/signup" underline="hover">
+                    <div>新規会員登録</div>
+                  </Link>
+                  <Link component={RouterLink} to="/login" underline="hover">
+                    <div>ログイン</div>
+                  </Link>
+                </>
+              )}
+            </Stack>
+          </Stack>
+        </Box>
+      </AppBar>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          sx={{
+            display: { xs: "block", sm: "block", md: "none" },
+            "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+    </>
   );
 });
