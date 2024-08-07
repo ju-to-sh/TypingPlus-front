7d4311930376c844184b4ae4f2ed3b4f970e783a
ju-to-sh <088t3748@gmail.com>
2024-08-07 22:41:01 +0900

    学習記録ページの作成

---
+++ app/src/components/organisms/study_record/TypingRecord.tsx
@@ -0,0 +1,56 @@
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { tableCellClasses } from "@mui/material/TableCell";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { FC, memo, useEffect } from "react";
+import { fetchtTypingResultSelector, typingResultState } from "../../../store/typingInfoState";
+import { useParams } from "react-router-dom";
+import { styled } from "@mui/material/styles";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: "#eee",
+    color: "#333",
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+export const TypingRecord: FC = memo(() => {
+  const { id } = useParams();
+  const fetchTyping = useRecoilValue(fetchtTypingResultSelector(id as string));
+  const [typingResult, setTypingResult] = useRecoilState(typingResultState(id as string));
+  // console.log(typingResult);
+
+  useEffect(() => {
+    setTypingResult(fetchTyping);
+  }, []);
+
+  return (
+    <TableContainer>
+      <Table sx={{ minWidth: 360 }} aria-label="spanning table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center" colSpan={3}>
+              最新のタイピング結果
+            </TableCell>
+          </TableRow>
+          <TableRow sx={{ border: "1px solid #eee" }}>
+            <StyledTableCell align="right">タイピング速度(wpm)</StyledTableCell>
+            <StyledTableCell align="right">ミスタイプ回数</StyledTableCell>
+            <StyledTableCell align="right">スコア</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {typingResult.typing && (
+            <TableRow sx={{ border: "1px solid #eee" }}>
+              <TableCell align="right">{typingResult.typing[0].attributes.type_speed}</TableCell>
+              <TableCell align="right">{typingResult.typing[0].attributes.miss_type}</TableCell>
+              <TableCell align="right">{typingResult.typing[0].attributes.score}</TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+});
--- app/src/types/api/typing.ts
+++ app/src/types/api/typing.ts
@@ -16,3 +16,21 @@ export type TypingInfo = {
   isMissType: boolean;
   missCount: number;
 };
+
+export type TypingResult = {
+  typing: Array<TypingResultData>;
+};
+
+export type TypingResultData = {
+  id: string;
+  type: string;
+  attributes: TypingResultAttributes;
+  relationships: Object
+};
+
+export type TypingResultAttributes = {
+  type_speed: number;
+  miss_type: number;
+  score: number;
+  created_at: string;
+};
