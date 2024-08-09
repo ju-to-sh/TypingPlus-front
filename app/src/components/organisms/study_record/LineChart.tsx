9f7db8bf0c3915ceddcb4c33759ef6af5314a404
ju-to-sh <088t3748@gmail.com>
2024-08-09 18:21:41 +0900

    タイピング結果グラフの実装

---
+++ app/src/components/organisms/study_record/LineChart.tsx
@@ -0,0 +1,119 @@
+import { Line } from "react-chartjs-2";
+import { Chart as ChartJS, LineElement, CategoryScale, LinearScale, Title, Tooltip, Legend } from "chart.js";
+import { FC, memo } from "react";
+import { TypingResultAttributes, TypingResultData } from "../../../types/api/typing";
+import _ from "lodash";
+
+ChartJS.register(LineElement, CategoryScale, LinearScale, Title, Tooltip, Legend);
+
+type Props = {
+  data: Array<TypingResultData>;
+};
+
+const TypingData = (data: Array<TypingResultData>) => {
+  return data?.map((ele) => ele.attributes);
+};
+const ImplementationArray = (data: Array<TypingResultAttributes>) => {
+  return data?.map((ele, index) => index + 1);
+};
+const TypingSpeedArray = (data: Array<TypingResultAttributes>) => {
+  const array = data?.map((ele) => _.pick(ele, ["type_speed"]));
+  return array?.map((item, index) => ({
+    times: index + 1,
+    type_speed: item.type_speed,
+  }));
+};
+const TypingMissTypeArray = (data: Array<TypingResultAttributes>) => {
+  const array = data?.map((ele) => _.pick(ele, ["miss_type"]));
+  return array?.map((item, index) => ({
+    times: index + 1,
+    type_speed: item.miss_type,
+  }));
+};
+const TypingScoreArray = (data: Array<TypingResultAttributes>) => {
+  const array = data?.map((ele) => _.pick(ele, ["score"]));
+  return array?.map((item, index) => ({
+    times: index + 1,
+    type_speed: item.score,
+  }));
+};
+
+export const LineChart: FC<Props> = memo((props) => {
+  const { data } = props;
+  const typingData = TypingData(data);
+  const labels = ImplementationArray(typingData);
+  const chartData = {
+    labels: labels,
+    type: "Line",
+    datasets: [
+      {
+        label: "タイピング速度(wpm)",
+        data: TypingSpeedArray(typingData),
+        borderColor: "rgb(83, 221, 106)",
+        yAxisID: "y",
+      },
+      {
+        label: "ミスタイプ回数",
+        data: TypingMissTypeArray(typingData),
+        borderColor: "rgb(97, 131, 242)",
+        yAxisID: "y1",
+      },
+      {
+        label: "スコア",
+        data: TypingScoreArray(typingData),
+        borderColor: "rgb(245, 152, 152)",
+        yAxisID: "y",
+      },
+    ],
+  };
+
+  const options: {} = {
+    maintainAspectRatio: false,
+    responsive: true,
+    plugins: {
+      tooltip: {
+        bodyFont: { size: 10 },
+        titleMarginBottom: 15,
+        backgroundColor: "#eee",
+        bodyColor: "#333",
+        displayColors: true,
+        xAlign: "center",
+        callbacks: {
+          title: () => "",
+        },
+      },
+    },
+    layout: {
+      padding: {
+        left: 10,
+        right: 50,
+      },
+    },
+    scales: {
+      x: {
+        display: true,
+        title: {
+          display: true,
+          text: "実施回数",
+        },
+      },
+      y: {
+        beginAtZero: true,
+      },
+      y1: {
+        display: true,
+        beginAtZero: true,
+        position: "right",
+        title: {
+          display: true,
+          text: "ミスタイプ回数",
+        },
+      },
+    },
+    parsing: {
+      xAxisKey: "times",
+      yAxisKey: "type_speed",
+    },
+  };
+  return <Line data={chartData} options={options} />;
+});
--- app/src/components/organisms/study_record/TypingRecord.tsx
+++ app/src/components/organisms/study_record/TypingRecord.tsx
@@ -1,10 +1,12 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import { tableCellClasses } from "@mui/material/TableCell";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { FC, memo, useEffect } from "react";
 import { fetchtTypingResultSelector, typingResultState } from "../../../store/typingInfoState";
 import { useParams } from "react-router-dom";
 import { styled } from "@mui/material/styles";
+import { Chart, registerables, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
+import { LineChart } from "./LineChart";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -16,40 +18,53 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
   },
 }));
 
+const divStyle: React.CSSProperties = {
+  marginLeft: "auto",
+  marginRight: "auto",
+  margin: "10px",
+  width: "100%",
+};
+
 export const TypingRecord: FC = memo(() => {
   const { id } = useParams();
   const fetchTyping = useRecoilValue(fetchtTypingResultSelector(id as string));
   const [typingResult, setTypingResult] = useRecoilState(typingResultState(id as string));
+  console.log(typingResult);
 
   useEffect(() => {
     setTypingResult(fetchTyping);
   }, []);
 
   return (
-    <TableContainer>
-      <Table sx={{ minWidth: 360 }} aria-label="spanning table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="center" colSpan={3}>
-              最新のタイピング結果
-            </TableCell>
-          </TableRow>
-          <TableRow sx={{ border: "1px solid #eee" }}>
-            <StyledTableCell align="right">タイピング速度(wpm)</StyledTableCell>
-            <StyledTableCell align="right">ミスタイプ回数</StyledTableCell>
-            <StyledTableCell align="right">スコア</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {typingResult.typing && (
-            <TableRow sx={{ border: "1px solid #eee" }}>
-              <TableCell align="right">{typingResult.typing[0].attributes.type_speed}</TableCell>
-              <TableCell align="right">{typingResult.typing[0].attributes.miss_type}</TableCell>
-              <TableCell align="right">{typingResult.typing[0].attributes.score}</TableCell>
+    <>
+      <Box style={divStyle}>
+        <LineChart data={typingResult.typing} />
+      </Box>
+      <TableContainer sx={{ display: "flex", justifyContent: "center" }}>
+        <Table sx={{ minWidth: 360, maxWidth: 500 }} aria-label="spanning table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center" colSpan={3}>
+                最新のタイピング結果
+              </TableCell>
             </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </TableContainer>
+            <TableRow sx={{ border: "1px solid #eee" }}>
+              <StyledTableCell align="right">タイピング速度(wpm)</StyledTableCell>
+              <StyledTableCell align="right">ミスタイプ回数</StyledTableCell>
+              <StyledTableCell align="right">スコア</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {typingResult.typing && (
+              <TableRow sx={{ border: "1px solid #eee" }}>
+                <TableCell align="right">{typingResult.typing.slice(-1)[0].attributes.type_speed}</TableCell>
+                <TableCell align="right">{typingResult.typing.slice(-1)[0].attributes.miss_type}</TableCell>
+                <TableCell align="right">{typingResult.typing.slice(-1)[0].attributes.score}</TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 });
